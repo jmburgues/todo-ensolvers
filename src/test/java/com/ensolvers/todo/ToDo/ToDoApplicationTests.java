62b3086d5b0a5cb648832beafab066746c93b179
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-04-27 09:46:56 -0300

    Initial commit. Adds Spring app.

---
+++ src/test/java/com/ensolvers/todo/ToDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensolvers.todo.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
